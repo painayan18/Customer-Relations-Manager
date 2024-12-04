641fb942d5d44528dec2c8955cd6a73e76386525
phant <nayanspai@gmail.com>
2024-12-04 16:37:58 -0600

    created homepage views and urls

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
-# Create your models here.
+class User(AbstractUser):
+    pass
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField(default=0)
+    agent = models.ForeignKey('Agent', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
