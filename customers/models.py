cfcd095d779edf7d77bfea246f62c70ae1f58aa3
phant <nayanspai@gmail.com>
2024-12-10 15:15:20 -0600

    modified agent customer model

--- customers/models.py
+++ customers/models.py
@@ -16,7 +16,12 @@ class Customer(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     age = models.IntegerField(default=0)
-    agent = models.ForeignKey('Agent', on_delete=models.CASCADE)
+    agent = models.ForeignKey(
+        'Agent',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
