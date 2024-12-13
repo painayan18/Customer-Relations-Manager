9bacae0b76cfa19b99f2e211d7632414181d6b30
nayanpai <nayanspai@gmail.com>
2024-12-13 13:09:19 -0600

    updated imports

--- customers/urls.py
+++ customers/urls.py
@@ -1,18 +1,24 @@
 from django.urls import path
-from . import views as cv # customer views
+from .views import (
+    CustomerListView, CustomerCreateView, CustomerDetailView,
+    CustomerUpdateView, CustomerDeleteView, AssignAgentView,
+    CategoryListView, CategoryCreateView, CategoryDetailView,
+    CategoryUpdateView, CategoryDeleteView, CustomerCategoryUpdateView
+)
+
 urlpatterns = [
-    path('', cv.CustomerListView.as_view(), name='customer-list'),
-    path('create/', cv.CustomerCreateView.as_view(), name='customer-create'),
-    path('<int:pk>/', cv.CustomerDetailView.as_view(), name='customer-detail'),
-    path('<int:pk>/update/', cv.CustomerUpdateView.as_view(), name='customer-update'),
-    path('<int:pk>/delete/', cv.CustomerDeleteView.as_view(), name='customer-delete'),
-    path('<int:pk>/assign-agent/', cv.AssignAgentView.as_view(), name='assign-agent'),
-    path('categories/', cv.CategoryListView.as_view(), name='category-list'),
-    path('create-category/', cv.CategoryCreateView.as_view(), name='category-create'),
-    path('categories/<int:pk>/update/', cv.CategoryUpdateView.as_view(), name='category-update'),
-    path('categories/<int:pk>/delete/', cv.CategoryDeleteView.as_view(), name='category-delete'),
-    path('category/<int:pk>/', cv.CategoryDetailView.as_view(), name='category-details'),
-    path('<int:pk>/category/', cv.CustomerCategoryUpdateView.as_view(), name='customer-category-update'),
+    path('', CustomerListView.as_view(), name='customer-list'),
+    path('create/', CustomerCreateView.as_view(), name='customer-create'),
+    path('<int:pk>/', CustomerDetailView.as_view(), name='customer-detail'),
+    path('<int:pk>/update/', CustomerUpdateView.as_view(), name='customer-update'),
+    path('<int:pk>/delete/', CustomerDeleteView.as_view(), name='customer-delete'),
+    path('<int:pk>/assign-agent/', AssignAgentView.as_view(), name='assign-agent'),
+    path('categories/', CategoryListView.as_view(), name='category-list'),
+    path('create-category/', CategoryCreateView.as_view(), name='category-create'),
+    path('categories/<int:pk>/update/', CategoryUpdateView.as_view(), name='category-update'),
+    path('categories/<int:pk>/delete/', CategoryDeleteView.as_view(), name='category-delete'),
+    path('category/<int:pk>/', CategoryDetailView.as_view(), name='category-details'),
+    path('<int:pk>/category/', CustomerCategoryUpdateView.as_view(), name='customer-category-update'),
 
 
 
