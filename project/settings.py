c598e0192b3f69ef5a617a15758e31c9113f250c
nayanpai <nayanspai@gmail.com>
2024-12-13 11:10:43 -0600

    added allowed host

--- project/settings.py
+++ project/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["crm-app-intelligent-giraffe-vg.cfapps.us10-001.hana.ondemand.com"]
 
 
 # Application definition
